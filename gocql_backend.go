571c8f845f077ccf32802df17c6995e7610619b9
indykish <nkishore@megam.io>
2016-02-15 16:43:04 +0530

    Make the default protocol version to 3.0 for scylla

--- gocql_backend.go
+++ gocql_backend.go
@@ -68,6 +68,7 @@ func GoCQLSessionToQueryExecutor(sess *gocql.Session) QueryExecutor {
 func newGoCQLBackend(nodeIps []string, username, password string) (QueryExecutor, error) {
 	cluster := gocql.NewCluster(nodeIps...)
 	cluster.Consistency = gocql.One
+	cluster.ProtoVersion = 3
 	cluster.Authenticator = gocql.PasswordAuthenticator{
 		Username: username,
 		Password: password,
